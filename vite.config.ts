5ee2d11e268e6b404578a7fc6e1a963ad662c2e8
Jiri Peterek <jiri@peterek.net>
2023-03-29 22:19:06 +0200

    build: handle dependencies as externals

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,24 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { dependencies } from './package.json'
+
+function getExternals(...sources: Record<string, string>[]) {
+  return [...new Set(sources.map(getFromDependencies).flat())]
+}
+
+function getFromDependencies(dependencies: Record<string, string>) {
+  return Object.keys(dependencies ?? {}).map((key) => new RegExp('^' + key))
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     target: "esnext",
+    rollupOptions: {
+      external: getExternals(dependencies)
+    }
   },
   resolve: {
     alias: {
